0def43f03c0983a9771ad395c730838295bf3f53
James Wilbraham <james@Jamess-MacBook-Pro.local>
2022-03-07 18:50:48 -0500

    Add TOML file

--- target/debug/deps/atty-a1a487e519ae26bd.d
+++
@@ -1,7 +0,0 @@
-/Users/james/projects/guessing_game/target/debug/deps/atty-a1a487e519ae26bd.rmeta: /Users/james/.cargo/registry/src/github.com-1ecc6299db9ec823/atty-0.2.14/src/lib.rs
-
-/Users/james/projects/guessing_game/target/debug/deps/libatty-a1a487e519ae26bd.rlib: /Users/james/.cargo/registry/src/github.com-1ecc6299db9ec823/atty-0.2.14/src/lib.rs
-
-/Users/james/projects/guessing_game/target/debug/deps/atty-a1a487e519ae26bd.d: /Users/james/.cargo/registry/src/github.com-1ecc6299db9ec823/atty-0.2.14/src/lib.rs
-
-/Users/james/.cargo/registry/src/github.com-1ecc6299db9ec823/atty-0.2.14/src/lib.rs:
--- target/debug/deps/colored-65af31805978a73d.d
+++
@@ -1,8 +0,0 @@
-/Users/james/projects/guessing_game/target/debug/deps/colored-65af31805978a73d.rmeta: /Users/james/.cargo/registry/src/github.com-1ecc6299db9ec823/colored-2.0.0/src/lib.rs /Users/james/.cargo/registry/src/github.com-1ecc6299db9ec823/colored-2.0.0/src/color.rs /Users/james/.cargo/registry/src/github.com-1ecc6299db9ec823/colored-2.0.0/src/control.rs /Users/james/.cargo/registry/src/github.com-1ecc6299db9ec823/colored-2.0.0/src/style.rs
-
-/Users/james/projects/guessing_game/target/debug/deps/colored-65af31805978a73d.d: /Users/james/.cargo/registry/src/github.com-1ecc6299db9ec823/colored-2.0.0/src/lib.rs /Users/james/.cargo/registry/src/github.com-1ecc6299db9ec823/colored-2.0.0/src/color.rs /Users/james/.cargo/registry/src/github.com-1ecc6299db9ec823/colored-2.0.0/src/control.rs /Users/james/.cargo/registry/src/github.com-1ecc6299db9ec823/colored-2.0.0/src/style.rs
-
-/Users/james/.cargo/registry/src/github.com-1ecc6299db9ec823/colored-2.0.0/src/lib.rs:
-/Users/james/.cargo/registry/src/github.com-1ecc6299db9ec823/colored-2.0.0/src/color.rs:
-/Users/james/.cargo/registry/src/github.com-1ecc6299db9ec823/colored-2.0.0/src/control.rs:
-/Users/james/.cargo/registry/src/github.com-1ecc6299db9ec823/colored-2.0.0/src/style.rs:
